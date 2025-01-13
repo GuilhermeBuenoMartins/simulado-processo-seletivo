9d63e1ca25644f5f1e7f372d17235f854a1897d7
Guilherme Bueno Martins <gui.bue@uninove.edu.br>
2025-01-13 00:58:43 +0000

    Criação do projeto Java.

---
+++ controle-pedido-api/src/test/java/br/exemplo/controle_pedido_api/ControlePedidoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.exemplo.controle_pedido_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePedidoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
